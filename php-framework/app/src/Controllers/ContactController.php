1e6539ef0fa417b4368dcb64bac9b960937b333f
Aijing LI <renoxevans@gmaiil.com>
2026-01-29 10:27:34 +0100

    feat: add /contact in routes

---
+++ php-framework/app/src/Controllers/ContactController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Controllers;
+
+use App\Lib\Controllers\AbstractController;
+use App\Lib\Http\Request;
+use App\Lib\Http\Response;
+
+class ContactController extends AbstractController{
+
+    public function process(Request $request): Response
+    {
+        // TODO: Implement process() method.
+    }
+}
